4158407b597124cc3165a88746dabb28def92615
Ryan Murry <ryanjmurry@gmail.com>
2018-03-06 13:30:19 -0800

    add js and html files for a user to input a number and will return the factorial of that number.

---
+++ js/master.js
@@ -0,0 +1,30 @@
+//Business Logic
+/*var factorial = function(number) {
+  if (number === 0) {
+    var total = 1
+    return total
+  } else {
+    for (index = 1; index <= number; index += 1) {
+      var total = 1*index;
+      return total
+    };
+  };
+};
+*/
+//User Interface Logic
+$(document).ready(function() {
+  $("form#calculator").submit(function(event) {
+    event.preventDefault();
+    //debugger
+    var number = parseInt($("input#number").val());
+    var total = 1
+    if (number === 0) {
+      total = 1
+    } else {
+      for (index = 1; index <= number; index += 1) {
+        total = total * index
+      }
+    }
+    alert(number + "! equals " + total);
+  });
+});
