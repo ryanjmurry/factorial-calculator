7519e7ec6e1fed19080c4a9546644dd73ea7f204
Ryan Murry <ryanjmurry@gmail.com>
2018-03-07 12:32:33 -0800

    separate business logic and user interface logic and will reload the page upon alerting the user of the value.

--- js/master.js
+++ js/master.js
@@ -1,30 +1,21 @@
-//Business Logic (currently not working)
-/*var factorial = function(number) {
+//Business Logic
+var total = 1
+var factorial = function(number) {
   if (number === 0) {
-    var total = 1
-    return total
+    total = 1
   } else {
     for (index = 1; index <= number; index += 1) {
-      var total = 1*index;
-      return total
+      total = total * index
     };
   };
 };
-*/
 //User Interface Logic
 $(document).ready(function() {
   $("form#calculator").submit(function(event) {
     event.preventDefault();
-    //debugger
     var number = parseInt($("input#number").val());
-    var total = 1
-    if (number === 0) {
-      total = 1
-    } else {
-      for (index = 1; index <= number; index += 1) {
-        total = total * index
-      }
-    }
+    factorial(number);
     alert(number + "! equals " + total);
+    location.reload();
   });
 });
